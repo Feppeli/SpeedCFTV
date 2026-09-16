1b6e811bd1a7defd93c542697f7de9a0ceb63feb
Douglas Felipe Teixeira da Silva <douglas.silva@gpk.local>
2026-09-16 15:33:28 -0300

    build: login form and create home screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
+import { useState } from 'react';
 import './App.css'
 import NavBar from './components/NavBar/NavBar'
+import { useNavigate } from 'react-router';
 
 function App() {
+
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    user: "",
+    password: ""
+  })
+
+  const userEnv = import.meta.env.VITE_USER
+  const passwordEnv = import.meta.env.VITE_PASSWORD
+
+  const handleChange = (e: any) => {
+    const {name, value} = e.target;
+    setFormData((prev) => ({...prev, [name]: value}))
+
+  }
+  
+  const handleSubmit = (e:any) => {
+    e.preventDefault()  
+    if(formData.user === userEnv && formData.password === passwordEnv){
+      navigate('/home')
+    }
+  }
 
   return (
     <>
@@ -9,12 +33,12 @@ function App() {
         <NavBar/>
         <section className='container'>
           <div className='loginForm'>
-            <form >
+            <form onSubmit={handleSubmit}>
               <h1>Login</h1>
-              <input type="text" name='user' placeholder='Usuário'/>
-              <input type="password" placeholder='Senha'/>
+              <input type="text" name='user' id='user' value={formData.user} onChange={handleChange} placeholder='Usuário'/>
+              <input type="password" name='password' id='password' value={formData.password} onChange={handleChange} placeholder='Senha'/>
               <div className='buttonDivisor'>
-                <button>Entrar</button>
+                <button type='submit'>Entrar</button>
               </div>
             </form>
           </div>
--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,16 @@ import { BrowserRouter, Routes, Route } from 'react-router'
 import './index.css'
 import App from './App.tsx'
 
+// PAGES
+import Home from './pages/home/home.tsx'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* <App /> */}
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<App/>}/>
+        <Route path='/home' element={<Home/>}/>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
