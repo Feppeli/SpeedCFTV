8282e196896fa1c4b25ad4b153a1834be4980af8
Douglas Felipe Teixeira da Silva <douglas.silva@gpk.local>
2026-09-24 17:23:49 -0300

    build: gen resume

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,29 +1,38 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import './Home.css'
 import NavBar from '../../components/NavBar/NavBar';
 
 
-const Home = () => {
+interface imageItem {
+    id: number,
+    date: string,
+    description: string,
+    startImage: string;
+    endImage: string;
+    nameCamera: string;
+}
 
+const Home = () => {
+    const [resumeModal, setResumeModal] = useState(false)
     const [openTicketModal, setOpenTicketModal] = useState(false);
-    const [ticketFormData, setTicketFormData]= useState({
-        id:0,
+    const [ticketFormData, setTicketFormData] = useState({
+        id: 0,
         date: "",
-        description:"",
-        startImage:"",
-        endImage:"",
-        nameCamera:""
+        description: "",
+        startImage: "",
+        endImage: "",
+        nameCamera: ""
     })
-    const [imagesDB, setImagesDB] = useState<Array<object>>([]);
+    const [imagesDB, setImagesDB] = useState<Array<imageItem>>([]);
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const {name, value} = e.target;
+        const { name, value } = e.target;
         setTicketFormData((prev) => ({
             ...prev, [name]: value
         }))
     }
 
-    const handleNewTicket = async ()  => {
+    const handleNewTicket = async () => {
         localStorage.setItem('Ticket', "true");
         setOpenTicketModal(true);
     }
@@ -33,25 +42,30 @@ const Home = () => {
         setOpenTicketModal(false)
     }
 
+    const handleOpenResumeModal = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault()
+        setResumeModal(!resumeModal)
+    }
+
     const handleAddImage = (e: React.MouseEvent<HTMLElement>) => {
         e.preventDefault()
 
         // VALIDAÇÃO DE CAMPOS VAZIOS
-        if(ticketFormData.nameCamera == ""){
+        if (ticketFormData.nameCamera == "") {
             alert("O campo: Nome da Câmera está em branco, Corrija e envie novamente")
         }
-        if(ticketFormData.date == ""){
+        if (ticketFormData.date == "") {
             alert("O campo: Nome da Câmera está em branco, Corrija e envie novamente")
         }
-        if(ticketFormData.startImage == ""){
+        if (ticketFormData.startImage == "") {
             alert("O campo: Hora Inicial está em branco, Corrija e envie novamente")
         }
-        if(ticketFormData.endImage == ""){
+        if (ticketFormData.endImage == "") {
             alert("O campo: Hora final está em branco, Corrija e envie novamente")
         }
 
         let data = localStorage.getItem('images')
-        if(data){
+        if (data) {
             setImagesDB(JSON.parse(data))
         }
 
@@ -75,12 +89,12 @@ const Home = () => {
         })
 
         setTicketFormData({
-            id:0,
+            id: 0,
             date: "",
-            description:"",
-            startImage:"",
-            endImage:"",
-            nameCamera:""
+            description: "",
+            startImage: "",
+            endImage: "",
+            nameCamera: ""
         })
 
         setOpenTicketModal(false)
@@ -92,15 +106,32 @@ const Home = () => {
         setImagesDB([])
         localStorage.setItem('images', '')
     }
- 
+
+    const formatArrayToText = (): string => {
+        if (imagesDB.length === 0) {
+            return "";
+        }
+
+        return imagesDB.map((image, i) => {
+            return (
+                `--- Imagem: ${i} ---\n` + '\n' +
+                `Câmera: ${image.nameCamera} \n` +
+                `Data: ${image.date} \n` +
+                `Início: ${image.startImage} \n` +
+                `Fim: ${image.endImage} \n` +
+                `Descrição: ${image.description != "" ? image.description : "*"} \n`
+            )
+        }).join('\n') // une tudo com uma quebra de linha extra
+    }
+
     return (
         <>
-            <NavBar/>
+            <NavBar />
             <section className='homeContainer'>
                 <div className='headerHomeSection'>
-                    {}
                     <button onClick={handleCleanImages}>Novo Relatório</button>
                     <button onClick={handleNewTicket}>Adicionar imagem</button>
+                    <button onClick={handleOpenResumeModal}>Gerar Relatório</button>
                 </div>
                 <div className='mainContent'>
                     <div>
@@ -108,18 +139,18 @@ const Home = () => {
                     </div>
                     <div className='imageContents'>
                         {imagesDB.map((img: any): any => {
-                            return(
-                            <div key={img.id} className='imageContent'>
-                                <div>                                
-                                    <p>ID: {img.id}</p>
-                                    <p>Câmera: {img.nameCamera}</p>
-                                    <p>Hora inicial: {img.startImage}</p>
-                                    <p>Hora Final: {img.endImage}</p></div>
-                                <div className='imageActions'>
-                                    <button>Editar</button>
-                                    <button>Excluir</button>
+                            return (
+                                <div key={img.id} className='imageContent'>
+                                    <div>
+                                        <p>ID: {img.id}</p>
+                                        <p>Câmera: {img.nameCamera}</p>
+                                        <p>Hora inicial: {img.startImage}</p>
+                                        <p>Hora Final: {img.endImage}</p></div>
+                                    <div className='imageActions'>
+                                        <button>Editar</button>
+                                        <button>Excluir</button>
+                                    </div>
                                 </div>
-                            </div>
                             )
                         })}
                     </div>
@@ -129,29 +160,43 @@ const Home = () => {
 
             {openTicketModal && (
                 <>
-                <div className='createTicket'>
-                    <div className='ticketModal'>
-                        <h2>Adicione Imagem ao relatório</h2>
-                        <input type="date" className='dateInput' placeholder='Data' name='date' value={ticketFormData.date} onChange={handleChange} />
+                    <div className='createTicket'>
+                        <div className='ticketModal'>
+                            <h2>Adicione Imagem ao relatório</h2>
+                            <input type="date" className='dateInput' placeholder='Data' name='date' value={ticketFormData.date} onChange={handleChange} />
 
-                        <input type="text" className='nameCameraInput' placeholder='nameCamera' name='nameCamera' value={ticketFormData.nameCamera} onChange={handleChange} />
+                            <input type="text" className='nameCameraInput' placeholder='nameCamera' name='nameCamera' value={ticketFormData.nameCamera} onChange={handleChange} />
 
-                        <div className='timeInputContainer'>
-                            <input type="text" className='startTimeInput' placeholder='Hora início' name='startImage' value={ticketFormData.startImage} onChange={handleChange} />
+                            <div className='timeInputContainer'>
+                                <input type="text" className='startTimeInput' placeholder='Hora início' name='startImage' value={ticketFormData.startImage} onChange={handleChange} />
 
-                            <input type="text" className='endTimeInput' placeholder='Hora Fim' name='endImage' value={ticketFormData.endImage} onChange={handleChange} />
+                                <input type="text" className='endTimeInput' placeholder='Hora Fim' name='endImage' value={ticketFormData.endImage} onChange={handleChange} />
 
+                            </div>
+                            <p>Descrição:</p>
+                            <input type="text" className="descriptionInput" placeholder='Digite aqui a descrição da imagem' name='description' value={ticketFormData.description} onChange={handleChange} />
+
+
+                            <button className='submitButton' onClick={handleAddImage}>Confirmar</button>
+                            <button className='exitButton' onClick={handleClose}>Cancelar</button>
                         </div>
-                        <p>Descrição:</p>
-                        <input type="text" className="descriptionInput" placeholder='Digite aqui a descrição da imagem' name='description' value={ticketFormData.description} onChange={handleChange} />
 
-
-                        <button className='submitButton' onClick={handleAddImage}>Confirmar</button>
-                        <button className='exitButton' onClick={handleClose}>Cancelar</button>
                     </div>
-
-                </div>
                 </>
+            )}
+
+            {resumeModal && (
+                <div className='resumeContainer'>
+                    <div className='resumeContent'>
+                        <div>
+                            <button>Copiar</button>
+                            <button onClick={handleOpenResumeModal}>Cancelar</button>
+                        </div>
+                        <div className='resumePreview'>
+                            <p style={{whiteSpace: 'pre-line'}}>{formatArrayToText()}</p>
+                        </div>
+                    </div>
+                </div>
             )}
         </>
     )
